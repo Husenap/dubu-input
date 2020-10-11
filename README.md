ea56fd40da423fb049bc482e4e60db4624e9c58e
Hussein Taher <hussein_taher@outlook.com>
2020-10-11 19:13:44 +0200

    updated readme

--- README.md
+++ README.md
@@ -13,6 +13,49 @@ C++ input library depending on [dubu-window](https://github.com/Husenap/dubu-win
 
 # Features
 
-
+* Map an arbitrary amount of keys/buttons to user actions
+* Register a listener to user action events
 
 # Examples
+
+##### **`Simple input mapping`**
+```cpp
+#include <iostream>
+
+#include <dubu_input/dubu_input.h>
+
+using namespace dubu::input;
+
+enum Actions {
+    Jump1,
+};
+
+int main() {
+    auto  window = dubu::window::GLFWWindow(400, 400, "dubu-input");
+    Input input(window);
+
+    // You can map action keys separately
+    input.MapAction(Actions::Jump1, Key::KeySpace);
+    input.MapAction(Actions::Jump1, GamepadInput{0, GamepadButton::GamepadButtonA});
+
+    // Or you can map an arbitrary amount of keys in the same `MapAction(...)` function call
+    input.MapAction(Actions::Jump1,
+                    Key::KeySpace,
+                    GamepadInput{0, GamepadButton::GamepadButtonA});
+
+    auto actionToken =
+        input.RegisterListener<UserAction>([&](const auto& action) {
+            switch (action) {
+            case Actions::Jump1:
+                std::cout << "Player 1: Jumping..." << std::endl;
+                break;
+            }
+        });
+
+    while (!window.ShouldClose()) {
+        window.PollEvents();
+
+        input.Update(window);
+    }
+}
+```
--- dubu_input_test/src/main.cpp
+++ dubu_input_test/src/main.cpp
@@ -6,34 +6,15 @@ using namespace dubu::input;
 
 enum Actions {
 	Jump1,
-	Shoot1,
-	Jump2,
-	Shoot2,
 };
 
 int main() {
-	auto window =
-	    std::make_unique<dubu::window::GLFWWindow>(400, 400, "dubu-input");
-	Input input(*window);
+	auto  window = dubu::window::GLFWWindow(400, 400, "dubu-input");
+	Input input(window);
 
-	input.MapAction(
-	    Actions::Jump1,
-	    Key::KeySpace,
-	    GamepadInput{.index = 0, .button = GamepadButton::GamepadButtonA});
-	input.MapAction(
-	    Actions::Shoot1,
-	    Key::KeyEnter,
-	    GamepadInput{.index  = 0,
-	                 .button = GamepadButton::GamepadButtonRightBumper},
-	    MouseButton::MouseButtonLeft);
-
-	input.MapAction(
-	    Actions::Jump2,
-	    GamepadInput{.index = 1, .button = GamepadButton::GamepadButtonA});
-	input.MapAction(
-	    Actions::Shoot2,
-	    GamepadInput{.index  = 1,
-	                 .button = GamepadButton::GamepadButtonRightBumper});
+	input.MapAction(Actions::Jump1,
+	                Key::KeySpace,
+	                GamepadInput{0, GamepadButton::GamepadButtonCross});
 
 	auto actionToken =
 	    input.RegisterListener<UserAction>([&](const auto& action) {
@@ -41,24 +22,12 @@ int main() {
 		    case Actions::Jump1:
 			    std::cout << "Player 1: Jumping..." << std::endl;
 			    break;
-		    case Actions::Shoot1:
-			    std::cout << "Player 1: Shooting..." << std::endl;
-			    break;
-		    case Actions::Jump2:
-			    std::cout << "Player 2: Jumping..." << std::endl;
-			    break;
-		    case Actions::Shoot2:
-			    std::cout << "Player 2: Shooting..." << std::endl;
-			    break;
-		    default:
-			    break;
 		    }
 	    });
 
-	while (!window->ShouldClose()) {
-		window->PollEvents();
+	while (!window.ShouldClose()) {
+		window.PollEvents();
 
-		input.Update(*window);
+		input.Update(window);
 	}
 }
-
